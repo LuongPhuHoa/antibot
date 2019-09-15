75774872e54682f826339f26158daf5e9ec94796
ʀᴀᴅᴇɴᴠᴏᴅᴋᴀ (ᴇᴋᴀ ꜱ) <mr.ekasyahwan@gmail.com>
2019-09-16 00:05:07 +0700

    Add files via upload

--- autoload.php
+++ autoload.php
@@ -6,7 +6,7 @@ session_start();
  * @Author: Eka Syahwan
  * @Date:   2018-11-14 15:36:23
  * @Last Modified by:   Nokia 1337
- * @Last Modified time: 2019-09-15 17:10:13
+ * @Last Modified time: 2019-09-16 00:03:21
 */
 require_once('lib/antibot.php');
 $Antibot = new Antibot; 
@@ -14,4 +14,5 @@ $Antibot = new Antibot;
 if(file_exists("htaccess.txt") && !file_exists("api/api.key")){
 	file_put_contents(".htaccess", file_get_contents("htaccess.txt"));
 }
+
 ?>
